fe515d38d95f012ed0218a9f838c928a68ead3a9
Ian Lee <ianlee1255@gmail.com>
2024-11-19 00:17:43 +0800

    added rainbowbrush & eraserbrush

--- etchnsketch.js
+++ etchnsketch.js
@@ -1,11 +1,19 @@
 
 // etch a sketch
 let side = 16;
+let brushMode = 'default';
 const container = document.querySelector("#container");
+
+//listen for buttons pressed
 let resetBtn = document.getElementById("reset");
 resetBtn.onclick=()=>resetCanvas();
 let resizeBtn = document.getElementById("resize");
 resizeBtn.onclick=()=>resizeCanvas();
+let rainbowBtn = document.getElementById('rainbow');
+rainbowBtn.onclick=()=>{brushMode='rainbow'};
+let eraserBtn = document.getElementById('eraser');
+eraserBtn.onclick=()=>{brushMode='eraser'};
+
 
 //listen for mouse drag over canvas
 let mouseDown = false;
@@ -19,6 +27,12 @@ function setupCanvas(side) {
         let item = document.createElement("div");
         item.className = "item";
         item.addEventListener('mouseover', changeColor);
+        item.addEventListener('click',()=>{
+            console.log("mousclick")
+            mouseDown=true;
+            changeColor(item);
+            mouseDown=false;
+        });
         item_size = String(100/side)+"%";
         item.style.flexBasis=item_size;
         container.appendChild(item);
@@ -28,6 +42,7 @@ function setupCanvas(side) {
 //reset canvas
 function resetCanvas() {
     console.log("reset canvas has been clicked") 
+    brushMode='default'
     container.innerHTML='';
     setupCanvas(side);
 }
@@ -41,20 +56,39 @@ function resizeCanvas() {
     }else{
         console.log(`INVALID input given,${tmp}`)
     }
-    
     console.log(`resize the canvas to ${tmp}`)
-
 }
 
 
 //color the canvas
-function changeColor(item,color="blue"){
+function changeColor(item){
     console.log(`mouseover box,mouseDown=${mouseDown}`);
-    if (mouseDown===true) {
-      item.target.style.backgroundColor=color;  
-      item.target.style.border=0;
-    };
+    if (mouseDown===true&&brushMode==='default') {
+        console.log("painting default brush");
+        item.target.style.backgroundColor='blue';    
+        item.target.style.border=0;
+    }else if(mouseDown===true&&brushMode==='rainbow'){
+        console.log("painting rainbow brush");
+        item.target.style.backgroundColor=rainbowBrush();  
+        item.target.style.border=0;
+    }else if(mouseDown===true&&brushMode==='eraser'){
+        console.log("painting eraser brush");
+        item.target.style.backgroundColor="whitesmoke";  
+        item.target.style.border="1px solid rgba(152, 148, 148, 0.243)";
+    }
 
 }
+
+//rainbow brush
+function rainbowBrush(){
+    let ran =Math.round( Math.random()*(255)).toString();
+    let ran2 =Math.round( Math.random()*(255)).toString();
+    let ran3 =Math.round( Math.random()*(255)).toString();
+    let rgbString = `rgb(${ran},${ran2},${ran3})`;
+    return rgbString
+}
+
+
+
 
 setupCanvas(side);
